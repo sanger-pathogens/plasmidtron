150aa6efb259582e45b1562540e4c7379f28fc14
andrewjpage <andrewjpage@gmail.com>
2017-01-11 11:50:28 +0000

    initial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ksieve
-Using kmers to find plasmids
+Check read trimminng before doing anything else with trimmomatic
+Small test set of reads
+kmc complex functionality
+What kmer do we use?
+
+
+#dependancies
+kmc version 2.3
+fastaq
+zcat
+awk
+spades 3.9.0
--- ksieve.py
+++ ksieve.py
@@ -7,18 +7,15 @@ import tempfile
 import subprocess
 
 class SampleData:
-    def __init__(self,forward_file, reverse_file):
-            self.forward_file = forward_file
-			self.reverse_file = reverse_file
-			self.database_name = ''
-			self.file_of_fastq_files = ''
-			self.basename=''
-			self.filtered_forward_file=''
-			self.filtered_reverse_file=''
-	
-
-
-
+	def __init__(self,forward_file, reverse_file):
+		self.forward_file = forward_file
+		self.reverse_file = reverse_file
+		self.database_name = ''
+		self.file_of_fastq_files = ''
+		self.basename=''
+		self.filtered_forward_file=''
+		self.filtered_reverse_file=''
+			
 # input data
 parser = argparse.ArgumentParser(
 	description = 'Plasmid kmers',
