f96d6a1e75515233582476165efad0d13d8ac9b8
andrewjpage <andrewjpage@gmail.com>
2017-01-11 15:12:21 +0000

    running script

--- ksieve.py
+++ ksieve.py
@@ -30,48 +30,48 @@ options = parser.parse_args()
 
 trait_samples = []
 nontrait_samples = []
-
 # read in the metadata about the fastq files
 with open(options.file_of_trait_fastqs) as csvfile:
 	spreadsheetreader = csv.reader(csvfile, delimiter = ',')
 	for row in spreadsheetreader:
-		forward_fastq_file = row[0]
-		reverse_fastq_file = row[1]
-		
-		if not os.path.exists(forward_fastq_file) or os.path.exists(reverse_fastq_file):
-			 sys.exit( "The input files dont exist")
-		trait_samples.append( SampleData(forward_fastq_file,reverse_fastq_file) )
+		forward_file = row[0]
+		reverse_file = row[1]
+
+		if not os.path.exists(forward_file) or not os.path.exists(reverse_file):
+			 sys.exit( "The input files do not exist: "+forward_file+ " "+reverse_file)
+		trait_samples.append( SampleData(forward_file,reverse_file) )
 
 # read in the metadata about the fastq files
 with open(options.file_of_nontrait_fastqs) as csvfile:
 	spreadsheetreader = csv.reader(csvfile, delimiter = ',')
 	for row in spreadsheetreader:
-		forward_fastq_file = row[0]
-		reverse_fastq_file = row[1]
-		
-		if not os.path.exists(forward_fastq_file) or os.path.exists(reverse_fastq_file):
-			 sys.exit( "The input files dont exist")
-		nontrait_samples.append( SampleData(forward_fastq_file,reverse_fastq_file) )
+		forward_file = row[0]
+		reverse_file = row[1]
+
+		if not os.path.exists(forward_file) or not os.path.exists(reverse_file):
+			 sys.exit( "The input files dont exist: ")
+		nontrait_samples.append( SampleData(forward_file,reverse_file) )
 
 # Run kmc to generate kmers for each set of FASTQs
 for set_of_samples in [trait_samples, nontrait_samples]:
 	for sample in set_of_samples:
 		temp_working_dir = tempfile.mkdtemp(dir=os.getcwd())
 		# TODO make sure to strip off path
-		basename = sample.forward_fastq_file.replace('_1.fastq.gz','')
-		sample.basename(basename)
-		
+		basename = sample.forward_file.replace('_1.fastq.gz','')
+		sample.basename = basename
+
 		with open(temp_working_dir+'/fofn', 'w') as file_of_sample_fastqs:
-			file_of_sample_fastqs.write(sample.forward_fastq_file + "\n")
-			file_of_sample_fastqs.write(sample.reverse_fastq_file + "\n")
-		
+			file_of_sample_fastqs.write(sample.forward_file + "\n")
+			file_of_sample_fastqs.write(sample.reverse_file + "\n")
+
 		database_name =  temp_working_dir+"/kmc_"+basename
 		file_of_fastq_files = temp_working_dir+"/fofn"
-		
-		sample.database_name(database_name)
-		sample.file_of_fastq_files(file_of_fastq_files)
-		
-		kmc_command = "kmc -ci20 -k"+options.kmer+" @"+file_of_fastq_files+" "+ database_name
+
+		sample.database_name = database_name
+		sample.file_of_fastq_files =file_of_fastq_files
+
+		kmc_command = "kmc -ci20 -k"+ str(options.kmer) +" @"+file_of_fastq_files+" "+ temp_working_dir+"/kmc_"+basename +" "+ temp_working_dir
+		print('DEBUG: '+ kmc_command)
 		subprocess.call(kmc_command,shell=True)
 
 # using Complex, create a file describing merging all the traits into one set, non traits into another set, then subtract.
@@ -81,28 +81,29 @@ temp_working_dir = tempfile.mkdtemp(dir=os.getcwd())
 complex_config_filename = temp_working_dir+'/complex_config_file'
 with open(complex_config_filename, 'w') as complex_config_file:
 	complex_config_file.write("INPUT:\n")
-	
+
 	# write out each database name
 	for set_of_samples in [trait_samples, nontrait_samples]:
 		for sample in set_of_samples:
-			complex_config_file.write(sample.basename+' = '+sample.database_name+"\n")
-	
+			complex_config_file.write(sample.basename.replace('#','_')+' = '+sample.database_name+"\n")
+
 	complex_config_file.write("OUTPUT:\n")
 	complex_config_file.write("result =")
 	trait_basenames = []
 	for sample in trait_samples:
-		trait_basenames.append(sample.basename)
+		trait_basenames.append(sample.basename.replace('#','_'))
 	nontrait_basenames = []
 	for sample in nontrait_samples:
-		nontrait_basenames.append(sample.basename)
-	
-	complex_config_file.write('('+  trait_basenames.join('+') +')')
+		nontrait_basenames.append(sample.basename.replace('#','_'))
+
+	complex_config_file.write('('+  '+'.join(trait_basenames) +')')
 	complex_config_file.write('-')
-	complex_config_file.write('('+  nontrait_basenames.join('+') +')')
+	complex_config_file.write('('+  '+'.join(nontrait_basenames) +')')
 	complex_config_file.write("\n")
 	# set operation
 
 kmc_complex_command = "kmc_tools complex " + complex_config_filename
+print('DEBUG: '+ kmc_complex_command)
 subprocess.call(kmc_complex_command,shell=True)
 
 
@@ -115,29 +116,31 @@ for sample in trait_samples:
 	temp_working_dir_filter = tempfile.mkdtemp(dir=os.getcwd())
 	intermediate_filtered_fastq = temp_working_dir_filter+'/intermediate.fastq'
 	read_names_file = temp_working_dir_filter+'/read_names_file'
-	
-	kmc_filter_command = 'kmc_tools filter results @'+sample.file_of_fastq_files+' '+intermediate_filtered_fastq
+
+	kmc_filter_command = 'kmc_tools filter result @'+sample.file_of_fastq_files+' '+intermediate_filtered_fastq
+	print('DEBUG: '+ kmc_filter_command)
 	subprocess.call(kmc_filter_command,shell=True)
-	
-	read_names_cmd = 'zcat '+intermediate_filtered_fastq + '| awk \'NR%4==1\' > ' + read_names_file
+
+	read_names_cmd = 'awk \'NR%4==1\' '+intermediate_filtered_fastq+' | sed \'s!@!!\' > ' + read_names_file
+	print('DEBUG: '+ read_names_cmd)
 	subprocess.call(read_names_cmd, shell=True)
 	
-	
 	filtered_forward_file = temp_working_dir_filter+'/sample_1.fastq.gz'
-	sample.filtered_forward_file(filtered_forward_file)
-	
+	sample.filtered_forward_file = filtered_forward_file
+
 	filtered_reverse_file = temp_working_dir_filter+'/sample_2.fastq.gz'
-	sample.filtered_reverse_file(filtered_reverse_file)
-	
+	sample.filtered_reverse_file = filtered_reverse_file
+
 	filtered_fastq_command = 'fastaq filter --ids_file '+read_names_file+' --mate_in '+sample.reverse_file+' --mate_out '+sample.filtered_reverse_file+' '+sample.forward_file+ ' '+sample.filtered_forward_file
+	print('DEBUG: '+ filtered_fastq_command)
 	subprocess.call(filtered_fastq_command, shell=True)
-	
+
 	# delete intermediate fastq file
 	os.remove(intermediate_filtered_fastq)
 	os.remove(read_names_file)
 
 for sample in trait_samples:
 	spades_output_directory = 'spades_'+sample.basename
-	spades_command = 'spades-3.9.0.py --careful --only-assembler -1 '+sample.forward_file+' -2 '+sample.reverse_file+' -o '+spades_output_directory
+	spades_command = 'spades-3.9.0.py --careful --only-assembler -1 '+sample.filtered_forward_file+' -2 '+sample.filtered_reverse_file+' -o '+spades_output_directory
 	subprocess.call(spades_command, shell=True)
-	
+	print('DEBUG: '+ spades_command)
