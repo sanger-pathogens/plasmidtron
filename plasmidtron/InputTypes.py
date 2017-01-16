1e60bcbf96b13b51c8cf649079e0137fbd137558
andrewjpage <andrewjpage@gmail.com>
2017-01-16 15:59:27 +0000

    check strings are digits rather than obj type

--- plasmidtron/InputTypes.py
+++ plasmidtron/InputTypes.py
@@ -19,28 +19,28 @@ class InputTypes:
 		return filename
 		
 	def is_kmer_valid(value_str):
-		if isinstance( value_str, int ):
+		if value_str.isdigit():
 			kmer = int(value_str)
 			if kmer%2 == 1 and kmer >= 21 and kmer <= 127:
 				return kmer
 		raise argparse.ArgumentTypeError("Invalid Kmer value, it must be an odd integer between 21 and 127")
 		
 	def is_min_contig_len_valid(value_str):
-		if isinstance( value_str, int ):
+		if value_str.isdigit():
 			min_contig_len = int(value_str)
 			if  min_contig_len >= 0 and min_contig_len <= 1000000:
 				return min_contig_len
 		raise argparse.ArgumentTypeError("Invalid minimum contig length value, try a resonable value like 600")
 	
 	def is_min_kmers_threshold_valid(value_str):
-		if isinstance( value_str, int ):
+		if value_str.isdigit():
 			min_kmers_threshold = int(value_str)
 			if  min_kmers_threshold >= 0 and min_kmers_threshold <= 255:
 				return min_kmers_threshold
 		raise argparse.ArgumentTypeError("Invalid minimum kmers threshold,t must be between 0 and 255, but ideally between 20-30.")
 		
 	def is_threads_valid(value_str):
-		if isinstance( value_str, int ):
+		if value_str.isdigit():
 			threads = int(value_str)
 			if  threads > 0 and threads <= 512:
 				return threads
